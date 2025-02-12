d9990a07da9d6d3298f61187d14a5573cd4fc299
Marcos Benítez Paredes <marcbenite.mb@gmail.com>
2025-02-11 21:51:33 -0300

    Creación del modelo user

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+//Esto es para interactuar con la base de datos
+export default mongoose.model("User", userSchema);
